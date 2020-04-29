97f31c9c5f9e391ba8f11cee0a9426570c4588e2
Keturah Howard <keturah.dev@gmail.com>
2020-04-29 11:33:39 -0700

    create and fail second test for setting a goalword.
    Co-authored-by: Alex Skreen <skreenalex@gmail.com>
    Co-authored-by: Miles Bradt <smbradtmichael@gmail.com>

--- src/__tests__/reducers/goal-word-reducer.test.js
+++ src/__tests__/reducers/goal-word-reducer.test.js
@@ -4,4 +4,22 @@ describe("goalWordReducer", () => {
   test("Should return default state", () => {
     expect(goalWordReducer({}, {type:null})).toEqual({})
   })
-})
+
+  let action;
+  const goalWord = {
+    word: "Testword"
+  };
+
+  test("Should set goal word", () => {
+    const { word } = goalWord;
+    action = {
+      type: 'SET_GOALWORD',
+      word: word
+    };
+
+    expect(goalWordReducer({}, action)).toEqual({
+      word: "Testword"
+    })
+  });
+});
+
