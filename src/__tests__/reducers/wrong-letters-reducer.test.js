64581eb4f3d4461fdeb3f733651d9b296ff371f5
Keturah Howard <keturah.dev@gmail.com>
2020-04-30 10:34:59 -0700

    pass test for listing out wrong letters.
    Co-authored-by: Miles Bradt <smbradtmichael@gmail.com>
    Co-authored-by: Alex Skreen <skreenalex@gmail.com>

--- src/__tests__/reducers/wrong-letters-reducer.test.js
+++ src/__tests__/reducers/wrong-letters-reducer.test.js
@@ -1,22 +1,42 @@
 import wrongLettersReducer from "./../../reducers/wrong-letters-reducer";
+// import v4 from 'uuid'
 
 describe("wrongLettersReducer", () => {
-  const userInput = "k"
+  // const userInput = "k"
+  // const userInput2 = "z"
+  const userInput = {letter: "k", id: 1}
+  const userInput2 = {letter: "z", id: 2}
   let action;
+
+  const state1 = {1: { letter: "k", id: 1}}
+
   test("Should return default state", () => {
     expect(wrongLettersReducer({}, {type: null})).toEqual({})
   })
   test("Should add wrong letter", () => {
+    const { letter, id} = userInput
+
     action = {
       type: "ADD_LETTER",
-      letter: userInput
+      letter: letter,
+      id: id
     }
-    expect(wrongLettersReducer({}, action)).toEqual({ "k": { letter: "k"} })
+    expect(wrongLettersReducer({}, action)).toEqual({ 1: { letter: "k", id: 1} })
   })
   test("Should return list of wrong letters", () => {
-    expect(wrongLettersReducer({}, action)).toEqual({
-      "k": { letter: "k"},
-      "z": { letter: "z"}
+    const { letter, id} = userInput2 
+    const action2 = {
+      type: "ADD_LETTER",
+      letter, //short hand for above
+      id
+    }
+    expect(wrongLettersReducer(state1, action2)).toEqual({
+      1: { letter: "k",
+            id: 1
+          },
+      2: { letter: "z",
+            id: 2
+          }
     })
   })
 })
--- src/reducers/wrong-letters-reducer.js
+++ src/reducers/wrong-letters-reducer.js
@@ -1,11 +1,30 @@
 export default (state={}, action) => {
-  const { letter } = action
+  const { letter, id } = action
   switch(action.type) {
     case "ADD_LETTER":
       return Object.assign({}, state, {
-        [letter]: {letter: letter}
-      })
+        [id]: {
+          letter: letter,
+          id: id
+        }
+      });
     default:
       return state
   }
 }
+// export default (state = {}, action) => {
+//   switch (action.type) {
+//   case 'ADD_TICKET':
+//     const { names, location, issue, id } = action;
+//     return Object.assign({}, state, {
+//       [id]: {
+//         names: names,
+//         location: location,
+//         issue: issue,
+//         id: id
+//       }
+//     });
+//   default:
+//     return state;
+//   }
+// };
