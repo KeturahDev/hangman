94b33675021cf1859083f582fa119885ec80a26e
Keturah Howard <keturah.dev@gmail.com>
2020-04-30 11:00:18 -0700

    create/fail test for toggle functionality on form vis reducer

--- src/__tests__/reducers/guess-form-visible-reducer.test.js
+++ src/__tests__/reducers/guess-form-visible-reducer.test.js
@@ -1,7 +1,12 @@
 import guessFormVisibleReducer from "./../../reducers/guess-form-visible-reducer"
 
 describe("guessFormVisibleReducer", () => {
+  let action;
   test("should return default state", () => {
     expect(guessFormVisibleReducer(false, {type:null})).toEqual(false)
+  })
+  test("should return true when toggle action is passed in", () => {
+
+    expect(guessFormVisibleReducer(false, action)).toEqual(true)
   })
 })
